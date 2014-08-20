4ca3724afddbd3a639362a5ccb8dadf1159a723d
dborstelmann <dborstelmann@pcp334001pcs.unl.edu>
2014-08-20 11:45:00 -0500

    hijoel

---
+++ huddl/Group Choice/huddl.swift
@@ -0,0 +1,20 @@
+//
+//  huddl.swift
+//  Group Choice
+//
+//  Created by Daniel Borstelmann on 8/20/14.
+//  Copyright (c) 2014 BorstelDiz Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class huddl: NSObject {
+    
+    var huddlName = ""
+    var id = ""
+    var group_id = ""
+    
+    init(hName: NSString) {
+        self.huddlName = hName
+    }
+}
