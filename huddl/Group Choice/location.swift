a7b4b5bdfc64235b3beee12c9df30ef87f571322
Daniel Borstelmann <dborstelmann@gmail.com>
2014-08-20 14:17:48 -0500

    changes

---
+++ huddl/Group Choice/location.swift
@@ -0,0 +1,13 @@
+//
+//  location.swift
+//  Group Choice
+//
+//  Created by Daniel Borstelmann on 8/20/14.
+//  Copyright (c) 2014 BorstelDiz Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class location: NSObject {
+   
+}
---
+++ huddl/Group Choice/vote.swift
@@ -0,0 +1,13 @@
+//
+//  vote.swift
+//  Group Choice
+//
+//  Created by Daniel Borstelmann on 8/20/14.
+//  Copyright (c) 2014 BorstelDiz Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class vote: NSObject {
+   
+}
